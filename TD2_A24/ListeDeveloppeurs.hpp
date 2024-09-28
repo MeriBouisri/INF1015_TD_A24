358ec747f4b1780f709c3de179e2b06f3e305783
Merieme Bouisri <meriemebouisri@gmail.com>
2024-09-27 23:10:52 -0400

    ADD : verification_allocation.hpp

--- TD2_A24/ListeDeveloppeurs.hpp
+++ TD2_A24/ListeDeveloppeurs.hpp
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include <cstddef>
 #include "Developpeur.hpp"
+#include "verification_allocation.hpp"
+#include "debogage_memoire.hpp"  //NOTE: Incompatible avec le "placement new", ne pas utiliser cette entête si vous utilisez ce type de "new" dans les lignes qui suivent cette inclusion.
 
 class ListeDeveloppeurs {
 public:

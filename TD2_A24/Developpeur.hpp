3b434deda6798696855b2ef9b349356d37f1911a
Merieme Bouisri <meriemebouisri@gmail.com>
2024-09-28 20:37:57 -0400

    FIX : Code coverage

--- TD2_A24/Developpeur.hpp
+++ TD2_A24/Developpeur.hpp
@@ -15,13 +15,13 @@ public:
 	void viderJeux();
 	int compterJeux(ListeJeux& jeux) const;
 	void actualiserJeux(ListeJeux& jeux);
-	void afficherJeux() const;
+	void afficherJeux();
 	static bool testDeveloppeur();
 	static bool testActualiserJeux();
 	static bool testCompterJeux();
 	static bool testViderJeux();
 	static bool testObtenirNom();
-	static bool testObtenirJeux();
+	static bool testAfficherJeux();
 
 
 private:

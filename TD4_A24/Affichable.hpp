6ce583862601c7ab41c480935909871eb7cbebb2
Jason Xa <jasonxa@icloud.com>
2024-11-04 16:59:29 -0500

    Implémentation de l'interface Affichage

---
+++ TD4_A24/Affichable.hpp
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+using namespace std;
+
+class Affichable {
+public:
+	virtual ostream& afficher(ostream& fluxSortie) = 0;
+	virtual ostream& changerCouleur(ostream& fluxSortie, int theme) = 0;
+};
+
+

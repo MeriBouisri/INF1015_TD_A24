a90d64eeb65030453dbd3afbe649565d33fd3220
Merieme Bouisri <meriemebouisri@gmail.com>
2024-11-08 20:16:22 -0500

    Initial commit (TD5)

---
+++ TD5_A24/Affichable.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <iostream>
+#include <array>
+using namespace std;
+
+enum class Theme { sombre, clair };
+class Affichable
+{
+public:
+	virtual ~Affichable() = default;  // Important!  Toute classe polymorphe doit avoir un destructeur virtuel, sinon la destruction par la classe de base est "undefined behavior".
+	virtual void afficher(ostream&) const = 0;
+	virtual void changerCouleur(ostream&, Theme) const = 0;
+
+	enum class Couleur { noir, rouge, vert, jaune, bleu, magenta, cyan, blancGris, noirGris, rougeIntense, vertIntense, jauneIntense, bleuIntense, magentaIntense, cyanIntense, blancIntense };
+	static void changerCouleurTexteFond(ostream& os, Couleur texte, Couleur fond) {  // Noter que cette méthode est statique et n'est donc pas une méthode de l'objet, donc généralement accepté comme pouvant être dans une interface; sinon simplement sortir la fonction de la classe.
+		static const array<int,16> enumCouleurVersAnsi = {30,31,32,33,34,35,36,37,90,91,92,93,94,95,96,97};
+		auto envoyerCodeCouleur = [&](int codeCouleurAnsi) { os << "\033[" << codeCouleurAnsi << "m"; };
+		envoyerCodeCouleur(0);
+		envoyerCodeCouleur(enumCouleurVersAnsi[int(texte)]);
+		envoyerCodeCouleur(enumCouleurVersAnsi[int(fond)]+10);
+	}
+};
---
+++ TD5_A24/Heros.hpp
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include "Personnage.hpp"
+#include "lireVectorDuFichier.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include "cppitertools/range.hpp"
+using namespace std;
+using namespace iter;
+
+class Heros : public virtual Personnage
+{
+public:
+	Heros() = default;  // Non nécessaire dans le TD4, pourrait servir dans le TD5.
+		
+	Heros(const string& nom, const string& jeu, const string& ennemi)  // Non nécessaire si on construit toujours en lisant du istream (TD4).
+		: Personnage(nom, jeu), ennemi_(ennemi) {}
+	
+	Heros(istream& fichier) : Personnage(fichier), ennemi_(lireString(fichier)), allies_(lireVectorDuFichier<string>(fichier)) {}
+	
+	void afficher(ostream& os) const override {
+		Personnage::afficher(os);
+		afficherSpecificites(os);
+	}
+	void afficherSpecificites(ostream& os) const {  // Pourrait être protected.
+		os << "Ennemi : " << ennemi_ << "\n";
+		os << "Alliés :\n";
+		for (auto&& allie : allies_)
+			os << "\t" << allie << "\n";
+	}
+	void changerCouleur(ostream& os, Theme theme) const override { // L'énoncé demande d'avoir des couleurs différentes pour les différentes classes, donc on override dans les classes...
+		switch (theme) {
+		case Theme::clair : changerCouleurTexteFond(os, Couleur::bleu, Couleur::blancIntense); break;
+		default :           changerCouleurTexteFond(os, Couleur::vertIntense, Couleur::noir); break;
+		}
+	}
+
+	// Les getters sont non nécessaires:
+	//const string& getEnnemi() const { return ennemi_; }
+	//const vector<string>& getAllies() const { return allies_; }
+
+private:
+	string ennemi_;
+	vector<string> allies_;
+};
